8127ff4372166c27e523d4e695c79a4f44d1fad7
wang_yi <yi.wang@dlr.de>
2023-10-19 14:46:04 +0200

    update test

--- src/example_package/example.py
+++ src/example_package/example.py
@@ -1,3 +1,27 @@
+import random
+
 def hello_world():
     print('hello, world!')
 
+
+def monte_carlo_integration(func, a, b, num_samples):
+    """
+    Compute the integral of a function using the Monte Carlo method.
+    
+    Parameters:
+    - func: The function to be integrated.
+    - a: The lower limit of the integration interval.
+    - b: The upper limit of the integration interval.
+    - num_samples: The number of random samples to use.
+
+    Returns:
+    - An estimate of the integral of the function.
+    """
+    total = 0
+    for _ in range(num_samples):
+        x = random.uniform(a, b)
+        total += func(x)
+    
+    average = total / num_samples
+    integral = (b - a) * average
+    return integral
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,15 @@
+import pytest
 def test_name():
   assert True
+
+from example_package.example import monte_carlo_integration
+
+
+def func(x):
+    return x
+
+@pytest.mark.parametrize("func,a,b,n,expected", [(func, 0,1,100, 0.5), (func, 0,2,100,2)])
+def test_mcint(func,a,b,n, expected):
+
+    assert abs(monte_carlo_integration(func, a, b, n) - expected) < 0.1 
+            
